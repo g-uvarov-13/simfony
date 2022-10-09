86a0ff456a312e8b435501cf62131ba1afc23250
g.uvarov <g.uvarov@test.com>
2022-10-09 19:38:28 +0500

    Реализована регистрастрация и авторизация.Закончен урок 4

---
+++ src/Form/ProfileEditFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileEditFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName',TextType::class,[
+                'label' => 'Введите ваше полное имя',
+                ])
+            ->add('phone',TextType::class,[
+                'label' => 'Введите номер телефона',
+            ])
+            ->add('address',TextType::class,[
+                'label' => 'Введите ваш адрес',
+            ])
+            ->add('zipcode',IntegerType::class,[
+                'label' => 'Введите ваш zipcode',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -47,7 +47,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-         return new RedirectResponse($this->urlGenerator->generate('main_homepage'));
+         return new RedirectResponse($this->urlGenerator->generate('main_profile_index'));
 
     }
 
